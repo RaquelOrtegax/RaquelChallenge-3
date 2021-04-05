38f81f2b9b56dba12f3c0ec5a11b84b2f640b8cf
RaqueOrtegax <raquelortega@hotmail.nl>
2021-04-05 20:12:14 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -114,7 +114,7 @@ function plotImageOnMap(icon, city){
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFxdWVsaGhzOTkiLCJhIjoiY2tuMzJxb3FxMDR6cjJ2bzBkd2N5amJpcSJ9.zLNyz6b18G7orncHQpmS3g';
     var weer = new mapboxgl.Map({
       container: 'weer',
-      style: 'mapbox://styles/raquelhhs99/ckn3334p50evj17mluogbwlgg',
+      style: 'mapbox://styles/raquelhhs99/ckn3334p50evj17mluogbwlgg', 
       center: [-87.661557, 41.893748],
       zoom: 10.7
     });
